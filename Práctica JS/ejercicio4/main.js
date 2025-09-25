08dea39132898582456229e14b9d46e93ae2748c
Pepe <jganavas@gmail.com>
2025-09-25 12:50:54 +0200

    ejercicios

--- Práctica JS/ejercicio4/main.js
+++ Práctica JS/ejercicio4/main.js
@@ -1,21 +1,48 @@
 let colores = ["red", "yellow", "green", "white", "blue", "brown", "pink", "black"];
 
 let franjas = prompt("¿Cuántas franjas tendrá la bandera?");
-while(franjas < 0 && franjas > 5){
+while((franjas < 0 && franjas > 5) || isNaN(franjas)){
     alert("Introduce un número válido (1-5)");
     franjas = prompt("¿Cuántas franjas tendrá la bandera?")
 }
 
 let array = [];
 for(let i = 0; i <= 5; i++){
-    array[i] += colores[Math.floor(Math.random()*colores.length)];
+    array[i] = colores[Math.floor(Math.random()*colores.length)];
 }
 
-for(let i = 0; i <= franjas; i++){
-    document.writeln(<table>
-        
+let cadena = "";
+let tag = "<table>";
+let tagCierre = "</table>";
+let franja = "<tr><td>Franja</td></tr>";
+let cadenaArray = [];
+let estiloArray = [];
+let color = "";
 
-    </table>);    
+for(let i = 0; i < franjas; i++){ 
+    cadenaArray.unshift(franja);
+
+    color += `tr:nth-child(${i+1}){background-color: ${array[Math.floor(Math.random()*array.length)]}\n }`;
+      
 }
+estiloArray = ["<style>"];
+estiloArray[1] = color;
+estiloArray.push("</style>");
 
 
+cadenaArray.unshift(tag);
+cadenaArray.push(tagCierre);
+cadena = cadenaArray.join("");
+cadena += estiloArray.join("");
+
+document.writeln(cadena);
+
+
+for(let i = 0; i < franjas; i++){ 
+    cadenaArray.unshift(franja);
+
+    color += `tr:nth-child(${i+1}){background-color: ${array[Math.floor(Math.random()*array.length)]}\n }`;
+
+    //PROBAR .INDEXOF
+      
+}
