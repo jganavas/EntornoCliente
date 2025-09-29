05e830f77bc56c54a8866d6192535d038a70db56
Pepe <jganavas@gmail.com>
2025-09-29 10:26:30 +0200

    ejs

--- Práctica JS/ejercicio4/main.js
+++ Práctica JS/ejercicio4/main.js
@@ -1,11 +1,11 @@
 let colores = ["red", "yellow", "green", "white", "blue", "brown", "pink", "black"];
-
+/*
 let franjas = prompt("¿Cuántas franjas tendrá la bandera?");
 while((franjas < 0 && franjas > 5) || isNaN(franjas)){
     alert("Introduce un número válido (1-5)");
     franjas = prompt("¿Cuántas franjas tendrá la bandera?")
-}
-
+}*/
+/*
 let array = [];
 for(let i = 0; i <= 5; i++){
     array[i] = colores[Math.floor(Math.random()*colores.length)];
@@ -18,13 +18,52 @@ let franja = "<tr><td>Franja</td></tr>";
 let cadenaArray = [];
 let estiloArray = [];
 let color = "";
-
+*/
+/*
+//OPCION A
 for(let i = 0; i < franjas; i++){ 
     cadenaArray.unshift(franja);
 
     color += `tr:nth-child(${i+1}){background-color: ${array[Math.floor(Math.random()*array.length)]}\n }`;
       
+}*/
+/*
+//OPCION B
+let arrayNoRep = [];
+let colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+
+for (let i = 0; i < franjas; i++) {
+    do{
+        colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+    }while(arrayNoRep.includes(colorAleatorio));
+
+    arrayNoRep.push(colorAleatorio);
 }
+
+for(let i = 0; i < franjas; i++){ 
+    cadenaArray.unshift(franja);
+
+    color += `tr:nth-child(${i+1}){background-color: ${arrayNoRep[i]}\n }`;
+      
+}
+*/
+//OPCION C
+let arrayC = [];
+let colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+
+for (let i = 0; i < 5; i++) {
+    do{
+        colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+    }while();
+    arrayC.push(colorAleatorio);
+}
+
+for (let index = 0; index < 5; index++) {
+    console.log(arrayC);
+}
+
+
+/*
 estiloArray = ["<style>"];
 estiloArray[1] = color;
 estiloArray.push("</style>");
@@ -36,13 +75,4 @@ cadena = cadenaArray.join("");
 cadena += estiloArray.join("");
 
 document.writeln(cadena);
-
-
-for(let i = 0; i < franjas; i++){ 
-    cadenaArray.unshift(franja);
-
-    color += `tr:nth-child(${i+1}){background-color: ${array[Math.floor(Math.random()*array.length)]}\n }`;
-
-    //PROBAR .INDEXOF
-      
-}
+*/
