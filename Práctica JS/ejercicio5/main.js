389c00dc31a85bfbefcdcc326f7ea5346e0993be
Pepe <jganavas@gmail.com>
2025-09-29 14:10:48 +0200

    n

--- Práctica JS/ejercicio5/main.js
+++ Práctica JS/ejercicio5/main.js
@@ -1,1 +1,20 @@
+let colores = ["red", "yellow", "green", "white", "blue", "brown", "pink", "black"];
+let palabras = ["hola", "yellow", "nose", "jaja", "lol", "bro", "pink", "jeee"];
 
+/*
+let palabras = [];
+for (let i = 0; i < 8; i++) {
+    
+    palabras[i] = prompt(`Introduce la palabra ${i+1}: `);
+    
+}*/
+
+for (let i = 0; i < 8; i++) {
+    let indice = palabras.indexOf(colores[i]);
+    if(indice != -1){
+        
+    }
+      
+}
+
+console.log(palabras);
