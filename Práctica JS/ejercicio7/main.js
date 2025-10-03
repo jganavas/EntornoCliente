c7d60fde4c5f682fa3eff30268beaa3dcd95ef04
Pepe <jganavas@gmail.com>
2025-10-03 20:57:08 +0200

    terminando

--- Práctica JS/ejercicio7/main.js
+++ Práctica JS/ejercicio7/main.js
@@ -1,19 +1,22 @@
 let nums = [];
+
+//ARRAY COMPROBACION CODIGO
 let numeros = [20, 5, 7, 4, 32, 9, 2, 14, 11, 6];
 
 /*for(let i = 0; i < 10; i++){
     nums[i] = prompt(`Introduce el número ${i+1}: `);
-    if(isNaN(nums[i]) || nums[i] < 0){
+    if(nums[i] < 0){
         nums[i] = prompt("Introduce un valor válido");
     }
 }*/
 
-let valores = "";
+/*let valores = "";
 for(let i = 0; i < 10; i++){
-    valores += `Valor: ${numeros[i]}  (Índice ${i})\n`;
+    valores += `Valor: ${nums[i]}  (Índice ${i})\n`;
 }
+alert(`Números introducidos: ${valores}\n`);
 
-/*
+
 let inicial = prompt("Introduce inicial");
 let final = prompt("Introduce final");
 let valido = inicial < final && inicial > 0 && final > 0 && inicial < 9 && final < 9; 
@@ -23,18 +26,33 @@ while(!valido){
     final = prompt("Introduce final");
 } 
 */
+
+//VARIABLES COMPROBACION
 let inicial = 3;
 let final = 7;
-let aux = 0;
-aux = numeros[final];
-numeros[final] = numeros[inicial];
-console.log(valores);
 
 
-/*for (let i = 0; i < 10; i++){
-    numeros[final] = numeros[(final+1)%10];
-    numeros[i] = numeros[i+1];
-}*/
+let aux = numeros[9];
+numeros.splice(final, 0, inicial+1);
+numeros.splice(inicial, 1);
+numeros.unshift(aux);
+
+//COMPROBACION CÓDIGO
+let valores = "";
+for(let i = 0; i < 10; i++){
+    valores += `Valor: ${numeros[i]}  (Índice ${i})\n`;
+}
+console.log(valores);    
+
+
+/*valores = "";
+for(let i = 0; i < 10; i++){
+    valores += `Valor: ${nums[i]}  (Índice ${i})\n`;
+}
+alert(`Resultado: ${valores}`);*/
+
+
+
 
 
 
---
+++ Práctica JS/ejercicio8/main.js
@@ -0,0 +1,45 @@
+let num_mesas = prompt("Cuántas mesas tendrá el restaurante?");
+//let num_mesas = 8;
+let mesas = [];
+let max_comensales = 4;
+
+for (let i = 0; i < num_mesas; i++) {
+    mesas[i] = Math.floor(Math.random()*4);
+}
+
+let comensales = 1;
+let sientese = "Por favor, siéntese en la mesa ";
+
+while(comensales != 0){
+    let encontrada = false;
+    comensales = prompt("Cuántas personas sois?");
+    if(comensales > max_comensales){
+        alert("Lo siento, no se admiten más de 4 personas por mesa");
+        comensales = prompt("Cuántas personas sois?");
+    }
+
+    for (let i = 0; i < num_mesas; i++) {
+        if(mesas[i] == 0){
+            mesas[i] = comensales;
+            alert(`${sientese} ${[i+1]}`);
+            encontrada = true;
+            break;
+        }else if((mesas[i] + comensales) <= 4){
+            mesas[i] += comensales;
+            alert(`${sientese} ${[i+1]}`);
+            encontrada = true;
+            break;
+        }else{
+            continue;
+        }
+    }
+    if(!encontrada){
+        alert("No hay mesas, por favor váyase a su casa queremos cerrar y descansar.");
+        break;
+    }
+}
+
+
+
+
+
