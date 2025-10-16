e757b861232583be5f264d1761613d442be0b814
Pepe <jganavas@gmail.com>
2025-10-16 12:52:39 +0200

    injusticias de la vida

--- Buscaminas/main.js
+++ Buscaminas/main.js
@@ -54,8 +54,6 @@ function contarMinasAdyacentes(tablero, fila, columna){
     return count;
 }
 
-//FUNCION mostrarCasillas (funcional) SIN RECURSIVIDAD
-/*
 function mostrarCasillasAdyacentes(tablero, fila, columna){
 
     let explosion = false;
@@ -64,67 +62,35 @@ function mostrarCasillasAdyacentes(tablero, fila, columna){
         explosion = true;
     }else{
         let count = 0;
+        //BUCLE FILAS
         for (let i = fila-1; i <= fila+1; i++) {
+            //MANTIENE ITERACIONES DENTRO DEL TABLERO
+            if(i < 0 || i > tablero[i].length){
+                i--;
+                continue;
+            } 
+            //BUCLE COLUMNAS
             for (let j = columna-1; j <= columna+1; j++) {
-                if(tablero[i][j] == contarMinasAdyacentes(tablero, fila, columna) == "0" || tablero[i][j] == contarMinasAdyacentes(tablero, fila, columna) == "*"){
-                    tablero[i][j] = contarMinasAdyacentes(tablero, i, j);
+                //MANTIENE ITERACIONES DENTRO DEL TABLERO
+                if(j < 0 || j > tablero[i].length){
+                    j--;
+                    continue;
+                }
+                //LOGICA Y FUNCIONAMIENTO
+                //Si la casilla no tiene bomba pero sí tiene una en su radio, se desvela sólo ella
+                if(tablero[fila][columna] != "*" && contarMinasAdyacentes(tablero, fila, columna) != "0"){
                     tablero[fila][columna] = contarMinasAdyacentes(tablero, fila, columna);
-                }else{
+                    break;
+                //Si no contiene ninguna bomba en su radio, desvela el radio    
+                }else if(contarMinasAdyacentes(tablero, fila, columna) == "0"){
                     tablero[i][j] = contarMinasAdyacentes(tablero, i, j);
-                }      
-            }
-        }
-    }
-    if(explosion){
-        return "BOOM";
-    }else{
-        return tablero;
-    }  
-}
-*/
-
-function mostrarCasillasAdyacentes(tablero, fila, columna){
-
-for (let i = fila-1; i <= fila+1; i++) {
-    if(i < 0 || i > tablero[i].length){
-        continue;
-    } 
-    for (let j = columna-1; j <= columna+1; j++) {
-        if(j < 0 || j > tablero[i].length){
-            continue;
-        }else if(tablero[i][j] == contarMinasAdyacentes(tablero, fila, columna) == "0"){
-            tablero = mostrarCasillasAdyacentes(tablero, i, j);
-        }else{
-            tablero[i][j] = contarMinasAdyacentes(tablero, i, j);
-        }      
-    }
-}
-return tablero; 
-}
-
-//PROBANDO RECURSIVIDAD
-/*
-function mostrarCasillasAdyacentes(tablero, fila, columna){
-
-    let explosion = false;
-
-    if(tablero[fila][columna] == "*"){
-        explosion = true;
-    }else{
-        let count = 0;
-        for (let i = fila-1; i <= fila+1; i++) {
-            if(i < 0 || i > tablero[i].length) continue;
-
-            for (let j = columna-1; j <= columna+1; j++) {
-                if(j < 0 || j > tablero[i].length) continue;
-
-                if(tablero[i][j] == contarMinasAdyacentes(tablero, fila, columna) == "*"){
-                    tablero[fila][columna] = contarMinasAdyacentes(tablero, fila, columna);
-                }else if(tablero[i][j] == contarMinasAdyacentes(tablero, fila, columna) == "0"){
+                }else if(tablero[i][j] == "*"){
+                    j--;
+                    continue;
+                //Si desvelando encuentra una casilla solitaria, vuelve a revelar su radio
+                }else if(tablero[i][j] == "0"){
                     tablero = mostrarCasillasAdyacentes(tablero, i, j);
-                }else{
-                    tablero[i][j] = contarMinasAdyacentes(tablero, i, j);
-                }      
+                }     
             }
         }
     }
@@ -134,53 +100,52 @@ function mostrarCasillasAdyacentes(tablero, fila, columna){
         return tablero;
     }  
 }
-*/
-
+/*
 let tablerov = generarTablero(5);
 tablerov = colocarMinas(tablerov, 4);
 console.log("TABLERO BASE: \n");
 console.table(tablerov);
 console.log("TABLERO ADMIN (jugando): \n");
 console.table(mostrarCasillasAdyacentes(tablerov, 2, 2));
-
+*/
 function mostrarTablero(tablero, fila, columna){
     let copiaTablero = [...tablero];
     for (let i = 0; i < tablero.length; i++) {
       for (let j = 0; j <= tablero[i].length; j++) {
         if(tablero[i][j] == "*" || tablero[i][j] === "0"){
-            copiaTablero[i][j] = "#";
+            copiaTablero[i][j] = "X";
         }
       }
     }
     return copiaTablero;
 }
+/*
 console.log("TABLERO USUARIO: \n");
 console.table(mostrarTablero(tablerov, 2, 2));
-
-function jugar(tablero, fila, columna){
-    if(tablero[fila][columna] == "*"){
-        explosion = true;
-    }else{
-        console.table(mostrarTablero(tablero, fila, columna));
-    }
-    return explosion;
-}
-
-/*
-//JUEGO
-let tamanio = prompt("Introduce el tamaño del tablero");
-let tablero = generarTablero(tamanio);
-
-let cantidad = prompt("Introduce la cantidad de bombas con las que deseas jugar");
-tablero = colocarMinas(tablero, cantidad);
-
-let explosion = false;
-while(!explosion){
-    let fila = prompt("Introduce posición X");
-    let columna = prompt("Introduce posición Y");
-    jugar(tablero, fila, columna);
-    if(jugar){
-        explosion = true;
-    }
-}
 */
+function jugar(){
+
+    let partidaAcabada = false;
+    //JUEGO
+    let tamanio = prompt("Introduce el tamaño del tablero");
+    let tablero = generarTablero(tamanio);
+
+    let cantidad = prompt("Introduce la cantidad de bombas con las que deseas jugar");
+    tablero = colocarMinas(tablero, cantidad);
+   
+    let explosion = false;
+    while(!explosion){
+        let fila = prompt("Introduce posición X");
+        let columna = prompt("Introduce posición Y");
+        console.table(mostrarCasillasAdyacentes(tablero, fila, columna));
+        console.table(mostrarTablero(tablero, fila, columna));
+        if(tablero[fila][columna] == "*"){
+            explosion = true;
+        }
+    }
+    
+}
+
+jugar();
+
+
