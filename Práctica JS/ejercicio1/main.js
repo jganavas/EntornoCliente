f454a666c709b5fa67bb6220a4291dae4e3a070b
Pepe <jganavas@gmail.com>
2025-10-24 13:02:14 +0200

    evito conflictos

--- Práctica JS/ejercicio1/main.js
+++ Práctica JS/ejercicio1/main.js
@@ -1,4 +1,5 @@
 //EJERCICIO 1
+/*
 var num = prompt("Introduce un número");
 while(num < 0 || isNaN(num)){
     num = prompt("Introduce un valor válido");
@@ -29,3 +30,17 @@ alert(`La suma de los cuadrados es ${sumaCuadrados}`);
 if(esCuadrado){
     alert(`La suma es el cuadrado del número ${n}`);
 }
+*/
+
+//VERSION ACTUALIZADA PARA TEMA 2
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Introduce numero 1", (num1) =>{
+    let num1 = num1;
+});
+
+
