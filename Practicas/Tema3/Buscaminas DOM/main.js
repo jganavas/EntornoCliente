5696c1a16a1edf433a3b72f72494e4805f209c15
Pepe <jganavas@gmail.com>
2025-11-27 12:48:02 +0100

    sigo

--- Practicas/Tema3/Buscaminas DOM/main.js
+++ Practicas/Tema3/Buscaminas DOM/main.js
@@ -7,6 +7,8 @@ var tamano;
 let contadorCeldas = 0;
 let numeroBombas = 8;
 let cantidadBanderas = numeroBombas;
+let contenedorTemporizador = document.getElementById("temporizador");
+let contenedorBanderas = document.getElementById("cantidadBanderas");
 
 let derrota;
 let victoria;
@@ -21,66 +23,51 @@ setTamanoTablero = function() {
 
     jugar();
 };
-
 jugar = function() {
     generarHTML();
+    setInterval(temporizador(), 1000);
 }
 
-//Evento para controlar a qué fila y columna ha clickado el usuario
-tablero.addEventListener("click", (e) => {
-    let fila = 0;
-    let columna = 0;
-    if(e.target.classList.contains("celda")){
-        fila = e.target.getAttribute("data-fila");
-        columna = e.target.getAttribute("data-columna");
-    }
-    logicaClick(e.target, fila, columna);
-});
+let contenedorFinDePartida = document.getElementById("finDePartida");
+let img = document.createElement("img");
 
 function logicaClick(celda, fila, columna){
     if(celda.innerText === "*"){
         derrota = true;
     }
+
     mostrarCasillasAdyacentes(tableroLogico, fila, columna);
+
     if(derrota){
         tablero.remove();
-        let contenedorBomba = document.getElementById("explosion");
-        let img = document.createElement("img");
-        
+        contenedorTemporizador.remove();
+        contenedorBanderas.remove();
         img.setAttribute("src", "./img/explosion.gif");
-        contenedorBomba.appendChild(img);
+
+        contenedorFinDePartida.appendChild(img);
     }else if(victoria){
         tablero.remove();
-        let contenedorVictoria = document.getElementById("victoria");
-        let img = document.createElement("img");
-
+        contenedorTemporizador.remove();
+        contenedorBanderas.remove();
         img.setAttribute("src", "./img/victoria.gif");
-        contenedorVictoria.appendChild(img);
+
+        contenedorFinDePartida.appendChild(img);
     }
 }
 
-//Evento para colocar banderas
-tablero.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
+function contarBanderas(){
+    let celdas = document.getElementsByClassName("celda"); 
+    let contadorBanderas = 0;
+    let tamanoTotal = tamano*tamano;
 
-    if(e.target.classList.contains("revelada")){
-        e.stopPropagation();
+    for (let i = 0; i < tamanoTotal; i++) {
+      if(celdas[i].classList.contains("bandera")){
+        contadorBanderas++;
+        contenedorBanderas.innerText = "Banderas restantes: " + (numeroBombas - contadorBanderas);
+      }
     }
-
-    cantidadBanderas--;
-    if(cantidadBanderas <= 0){
-        alert("No puedes usar más banderas");
-    }
-
-    e.target.classList.toggle("bandera");
-    e.target.classList.toggle("oculta");
-});
-
-tablero.addEventListener("dblclick", (e) => {
-    if(e.target.classList.contains("bandera")){
-        e.target.classList.remove("bandera");
-    }
-});
+    return contadorBanderas;
+}
 
 function generarHTML(){
     
@@ -135,9 +122,16 @@ querySelector(`div[data-fila='${fila}'], div[data-columna='${columna}']`);
 function revelarCelda(fila, columna){
     let celda = document.querySelector(`div[data-fila='${fila}'][data-columna='${columna}']`);
     
+    let tamanoTotal = tamano*tamano;
+
     celda.classList.remove("oculta");
     celda.classList.add("revelada");
-    //console.log(`Fila: ${fila}, Columna: ${columna}`);
+
+    contadorCeldas++;
+
+    if(contadorCeldas >= tamanoTotal - numeroBombas){
+        victoria = true;
+    }
 }
 
 function mostrarCasillasAdyacentes(tablero, fila, columna){
@@ -170,16 +164,9 @@ function mostrarCasillasAdyacentes(tablero, fila, columna){
     }
     
     revelarCelda(fila, columna);
-    contadorCeldas++;
-    //console.log(`Log 1: Fila: ${fila}, Columna: ${columna}`);
-
-    //Si tiene minas alrededor revelo el número de minas adyacentes sin mostrar su alrededor
-    if(celdita.innerText != "0"){
-        revelarCelda(fila, columna);
-        
-        //console.log(`Log 2: Fila: ${fila}, Columna: ${columna}`);
+    
     //Si es una celda solitaria, revelo su alreddor
-    }else if(celdita.innerText == "0"){
+    if(celdita.innerText == "0"){
 
         for (let i = fila-1; i <= fila+1; i++) {
             //Mantengo fila en los límites
@@ -195,15 +182,29 @@ function mostrarCasillasAdyacentes(tablero, fila, columna){
                 if(i == fila && j == columna){
                     continue;
                 }
-                contadorCeldas++;
                 mostrarCasillasAdyacentes(tablero, i, j);
             }
         }
     }
+}
 
-    if(contadorCeldas >= tamanoTotal - numeroBombas){
-        victoria = true;
+let minutos = 4;
+let segundos = 60;
+
+function temporizador(){
+    segundos--;
+    if(segundos < 0){
+        minutos--;
+        segundos = 60;
     }
+
+    if(minutos === 0 && segundos === 0){
+        alert("Te has quedado sin tiempo. Espabila");
+    }
+    
+    let tiempo = `${minutos}:${segundos}`;
+
+    contenedorTemporizador.textContent = tiempo;
 }
 
 function generarTableroLogico(){
@@ -276,234 +277,45 @@ function generarTableroLogico(){
     return tablero;
 }
 
-/* CÓDIGO BUSCAMINAS DE CONSOLA */ 
-
-/* 
-function generarTablero(tamano){
-    // Bucle para crear las filas
-    let tablero = [];
-    for(let i = 0; i < tamano; i++){
-        let fila = [];
-        // Bucle para crear las columnas
-        for(let j = 0; j < tamano; j++){
-            let columna = "0";
-            fila.push(columna);
-        }
-        tablero.push(fila);
+//Evento para controlar a qué fila y columna ha clickado el usuario
+tablero.addEventListener("click", (e) => {
+    let fila = 0;
+    let columna = 0;
+    if(e.target.classList.contains("celda")){
+        fila = e.target.getAttribute("data-fila");
+        columna = e.target.getAttribute("data-columna");
     }
+    logicaClick(e.target, fila, columna);
+});
 
-    return tablero;
-}
+//Evento para colocar banderas
+tablero.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
 
-function colocarMinas(tablero, cantidad){
-    let count = 0;
-
-    while(count < cantidad){
-        //Genero aleatoriamente posiciones para colocar las minas
-        let minaX = Math.floor(Math.random()*tablero.length);
-        let minaY = Math.floor(Math.random()*tablero.length);
-        
-        //Me aseguro que no vuelvo a colocar una mina donde ya había una colocada
-        if(tablero[minaX][minaY] == "*"){
-            continue;
-        }
-        
-        tablero[minaX][minaY] = "*";
-        
-        count++;
+    if(e.target.classList.contains("revelada")){
+        e.stopPropagation();
     }
-
-    return tablero;
-}
-
-function generarTableroLogico(tablero){
-
-    //Recorro el tablero
-    for (let i = 0; i < tablero.length; i++) {
-        for (let j = 0; j < tablero.length; j++) {
-            //Si la columna es una mina, continúo
-            if(tablero[i][j] == "*"){
-                continue;
-            }
-            let cont = 0;
-
-            //LOGICA PARA ASIGNAR A CADA CELDA SU NUMERO CORRESPONDIENTE
-            for (let k = -1; k <= 1; k++) {
-                for (let r = -1; r <= 1; r++) {
-                    //Ignoro la celda en la que se acciona el bucle
-                    if(k == 0 && r == 0){
-                        continue;
-                    }
-                    //Se calcula la posición relativa en la que estaría la celda
-                    let fila = i + k;
-                    let columna = j + r;
-
-                    if (fila >= 0 && fila < tablero.length && columna >= 0 && columna < tablero.length){
-                        if(tablero[fila][columna] == "*"){
-                            cont++;
-                        }
-                    }
-                }
-            }
-            tablero[i][j] = cont;
-        }
-    }
-
-    return tablero;
-}
-
-function contarMinasAdyacentes(tablero, fila, columna){
-
-    let cont = 0;
-    for (let i = fila-1; i <= fila+1; i++) {
-        //Mantengo fila en los límites
-        if(i < 0 || i >= tablero.length){
-            continue;
-        } 
-        for (let j = columna-1; j <= columna+1; j++) {
-            //Mantengo columna en los límites
-            if(j < 0 || j >= tablero.length){
-                continue;
-            } 
-            if(tablero[i][j] == "*"){
-                cont++;
-            }
-        }
-    }
-
-    return cont;
-}
-
-function generarTableroCopia(tamano){
-    // Bucle para crear las filas
-    let tableroCopia = [];
-    for(let i = 0; i < tamano; i++){
-        let fila = [];
-        // Bucle para crear las columnas
-        for(let j = 0; j < tamano; j++){
-            let columna = "X";
-            fila.push(columna);
-        }
-        tableroCopia.push(fila);
-    }
-
-    return tableroCopia;
-}
-
-function mostrarCasillasAdyacentes(tablero, tableroCopia, fila, columna){
-
-    let filaCorrecta = fila >= 0 && fila < tablero.length && !isNaN(fila);
-    let columnaCorrecta = columna >= 0 && columna < tablero.length && !isNaN(columna);
-
-    //CONDICION DE SALIDA RECURSIVDAD
-    if(!filaCorrecta || !columnaCorrecta){
-        return;
-    }
-
-    //CONDICION DE SALIDA RECURSIVIDAD
-    if(tableroCopia[fila][columna] != "X"){
-        return;
-    }
-    
-    tableroCopia[fila][columna] = tablero[fila][columna];
-
-    //Si tiene minas alrededor revelo el número de minas adyacentes sin mostrar su alrededor
-    if(tablero[fila][columna] != "0"){
-        tableroCopia[fila][columna] = tablero[fila][columna];
-    //Si es una celda solitaria, revelo su alreddor
-    }else if(tablero[fila][columna] == "0"){
-
-        for (let i = fila-1; i <= fila+1; i++) {
-            //Mantengo fila en los límites
-            if(i < 0 || i >= tablero.length){
-                continue;
-            } 
-            for (let j = columna-1; j <= columna+1; j++) {
-                //Mantengo columna en los límites
-                if(j < 0 || j >= tablero.length){
-                    continue;
-                }
-                //Ignoro la celda en la que se acciona el bucle
-                if(i == fila && j == columna){
-                    continue;
-                }
-                
-                mostrarCasillasAdyacentes(tablero, tableroCopia, i, j);
-            }
-        }
-    }
-}
-
-function mostrarTablero(tableroCopia){
-    console.table(tableroCopia);
-}
-
-function esVictoria(tablero, tableroCopia){
-
-    let esVictoria = true;
-
-    for(let i = 0; i < tablero.length; i++){
-        for(let j = 0; j < tablero.length; j++){
-            if(tablero[i][j] != "*" && tableroCopia[i][j] == "X"){
-                return false;
-            }
-        }
-    }
-    return esVictoria;
-}
-
-function jugar(){
-
-    //Valores iniciales
-    let tamano = parseInt(prompt("Introduce tamano del tablero"));
-    let minas = parseInt(prompt("Introduce numero de minas"));
-
-    //Variables de tablero
-    let tablero = generarTablero(tamano);
-    tablero = colocarMinas(tablero, minas);
-    tablero = generarTableroLogico(tablero);
-    tableroCopia = generarTableroCopia(tamano);
-
-    //Bucle de juego
-    let explosion = false;
-    while(!explosion){
-
-        let fila = parseInt(prompt("Introduce fila"));
-        let columna = parseInt(prompt("Introduce columna"));
-
-        let filaCorrecta = fila >= 0 && fila < tamano && !isNaN(fila);
-        let columnaCorrecta = columna >= 0 && columna < tamano && !isNaN(columna);
-
-        if(!filaCorrecta || !columnaCorrecta){
-            alert("Introduce valores válidos");
-            continue;
-        }
-
-        if(tableroCopia[fila][columna] != "X"){
-            alert("Este valor ya ha sido utilizado");
-            continue;
-        }
-
-        if(tablero[fila][columna] == "*"){
-            alert("BOOM");
-            explosion = true;   
-            mostrarTablero(tablero);
-            console.log("Fueras estudiao");       
+    else{
+        let contadorBanderas = contarBanderas();
+        if(contadorBanderas < numeroBombas){
+            contadorBanderas--;
+            e.target.classList.toggle("bandera");
+            e.target.classList.toggle("oculta");
         }else{
-            mostrarCasillasAdyacentes(tablero, tableroCopia, fila, columna);
-            mostrarTablero(tableroCopia);
-
-            if(esVictoria(tablero, tableroCopia)){
-                alert("Has ganado :)");
-                break;
-            }
+            alert("No puedes usar más banderas");
         }
     }
-}
-*/
+
+});
+
+tablero.addEventListener("dblclick", (e) => {
+    if(e.target.classList.contains("bandera")){
+        e.target.classList.remove("bandera");
+        cantidadBanderas++;
+    }
+});
 
 botonEnviar.addEventListener("click", setTamanoTablero);
 
-//alert(numeroDeMinas);
 
 
